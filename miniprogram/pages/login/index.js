0662da2316f75b3706c9a98f29db76c5474fcd3b
刘小伟 <liuxiaowei@weixin002.local>
2026-08-16 00:48:24 +0800

    feat: 样板页 01 登录 1:1 还原

--- miniprogram/pages/login/index.js
+++ miniprogram/pages/login/index.js
@@ -1,3 +1,14 @@
+const { toast, navigate } = require("../../utils/interaction");
+
 Page({
-  data: {}
+  data: { agreed: true },
+  toggleAgree() {
+    const agreed = !this.data.agreed;
+    this.setData({ agreed });
+    toast(agreed ? "已同意《用户协议》和《隐私政策》" : "需同意协议后才能使用人脸相关功能");
+  },
+  onLogin() {
+    // 真实微信登录（wx.login/手机号快捷登录）待后端接口就绪后接入
+    navigate("/pages/basic-info/index");
+  }
 });
